bf52f0722c0957d2aa52cbd5491aec9478bd9f89
manoj <manojkdm184509@gmail.com>
2021-04-13 13:39:06 +0530

    first commit

---
+++ demoapp/src/main/java/com/demoapp/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.demoapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/demo")
+	public String demo() {
+		return "demo";
+	}
+}
